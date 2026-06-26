4072129968c69e9fbe261fce0846b67c4b12d26d
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-06-26 13:13:00 -0400

    Oops

--- service.nix
+++ service.nix
@@ -19,7 +19,7 @@ let
   
   launch-script = pkgs.writeShellScriptBin "launch-script" ''
     set -euxo pipefail
-    exec ${package}/bin/resonite-dominion --port ${builtins.toString cfg.port} --shutdown-seconds ${builtins.toString cfg.shutdown-seconds}
+    exec ${package}/bin/resonite-dominion --port ${builtins.toString cfg.port} --shutdown-seconds ${builtins.toString cfg.shutdown-seconds} daemon
   '';
 in
 {
